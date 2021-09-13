d12a06e4861ba8bc1d93e11db6f4cfa6220ac327
Farhan Hasan <hasanfarhan33@gmail.com>
2021-09-13 22:33:31 +0300

    Initial Commit

---
+++ matrixPlot.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_theme()
+
+tips = sns.load_dataset('tips')
+flights = sns.load_dataset('flights')
+
+# print(tips.head())
+# print("")
+# print(flights.head())
+
+tc = tips.corr()
+
+# HEAT MAPS
+# sns.heatmap(tc, annot=True, cmap='coolwarm')
+
+fc = flights.pivot_table(index='month', columns='year', values='passengers')
+sns.heatmap(fc)
+
+
+
+plt.show()
